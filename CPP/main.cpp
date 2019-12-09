fd646f9b1795465f78725cd194efc762a4be3cce
William Martin <35669235+cominatyou@users.noreply.github.com>
2019-12-09 15:53:19 -0600

    CPP Bad

--- CPP/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-#include "types.cpp"
-
-int main(){
-	std::cout << "Hey there\n";
-}
--- CPP/types.cpp
+++
@@ -1,7 +0,0 @@
-//nice file
-
-struct fpos{
-float x = 0f;
-float y = 0f;
-float z = 0f;
-};
